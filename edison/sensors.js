c1ebac505594e2e4d571667644c0d1641831f6e1
Esther Jun Kim <esther.jun.kim@gmail.com>
2016-04-07 23:21:55 +0000

    Add socket.io client to send sensor data to web server

--- edison/sensors.js
+++ edison/sensors.js
@@ -1,5 +1,8 @@
 var noble = require('noble');
 
+// MODIFY THIS WITH THE APPROPRIATE URL
+var socket = require('socket.io-client')('http://drejkim-mbpr.local:8080');
+
 var IMU_SERVICE_UUID = '2947ac9efc3811e586aa5e5517507c66';
 var AX_CHAR_UUID =  '2947af14fc3811e586aa5e5517507c66';
 var AY_CHAR_UUID = '2947b090fc3811e586aa5e5517507c66';
@@ -7,6 +10,12 @@ var AZ_CHAR_UUID = '2947b180fc3811e586aa5e5517507c66';
 var GX_CHAR_UUID = '2947b252fc3811e586aa5e5517507c66';
 var GY_CHAR_UUID = '2947b5aefc3811e586aa5e5517507c66';
 var GZ_CHAR_UUID = '2947b694fc3811e586aa5e5517507c66';
+
+socket.on('connect', function() {
+  console.log('Connected to server');
+
+  socket.emit('hello');
+});
 
 noble.on('stateChange', function(state) {
   if(state === 'poweredOn') {
@@ -32,6 +41,7 @@ noble.on('discover', function(peripheral) {
 
         axChar.on('read', function(data) {
           console.log(data.readInt32LE(0));
+          socket.emit('ax:edison', data.readInt32LE(0));
         });
 
         axChar.notify('true', function(error) { if (error) throw error; });
